7280972a03ac61230f185ac6757604c8651424e5
Henry Veliz <hveliz791@gmail.com>
2026-02-23 12:21:01 -0600

    mensaje describiendo el cambio

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,12 @@ async function bootstrap() {
     credentials: true,
   });
 
+  // SI tu frontend llama /api/..., descomenta esto:
+  // app.setGlobalPrefix('api');
+
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
-  await app.listen(process.env.PORT || 3000);
+  const port = Number(process.env.PORT) || 3000;
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
